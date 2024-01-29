d16e87a067a854eb0ea170ca532b8c1c5e7770a3
nurai003002 <nuraj9663@gmail.com>
2024-01-29 18:57:24 +0600

    travel

--- apps/tours/serializers.py
+++ apps/tours/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 
 from apps.tours.models import Tour
+from apps.hotels.serializers import HotelSerializer
 
 class TourSerializers(serializers.ModelSerializer):
+    tour_hotels = HotelSerializer(read_only=True, many=True)
     class Meta:
         model = Tour
         fields = '__all__'
